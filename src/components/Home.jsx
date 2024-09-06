80870816eb197a6863db8113a64c559bfad82aeb
Farhad Afridi <afridifarhad149@gmail.com>
2024-09-06 15:50:48 +0500

    add resopnsive design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,21 +8,19 @@ function Home() {
             id: 1,
             imgUrl: "https://plus.unsplash.com/premium_photo-1673439304183-8840bd0dc1bf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGl6emF8ZW58MHx8MHx8fDA%3D",
             name: "Pizza"
-            
-        } ,
+        },
         {
             id: 2,
             imgUrl: "https://images.unsplash.com/photo-1485962398705-ef6a13c41e8f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZvb2QlMjBpbWFnZXMlMjBidXJndXJ8ZW58MHx8MHx8fDA%3D",
             name: "Pasta"
-            
         },
-         {
+        {
             id: 3,
             imgUrl: "https://images.unsplash.com/photo-1552611052-d59a0d9741bc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmVzdGF1cmFudCUyMGZvb2QlMjBpbWFnZXN8ZW58MHx8MHx8fDA%3D",
             name: "Food"
-            
         }
-    ]
+    ];
+
     const menuCards = [
       {
         id: 1,
@@ -38,7 +36,7 @@ function Home() {
       },
       {
         id: 3,
-        name: "K2 Burgur",
+        name: "K2 Burger",
         price: "580",
         imgURL: "https://images.pexels.com/photos/327158/pexels-photo-327158.jpeg?auto=compress&cs=tinysrgb&w=600"
       },
@@ -48,52 +46,58 @@ function Home() {
         price: "230",
         imgURL: "https://images.pexels.com/photos/8471703/pexels-photo-8471703.jpeg?auto=compress&cs=tinysrgb&w=600"
       }
-    ]
+    ];
+
   return (
     <div>
+      {/* Hero Section */}
       <section className="relative">
         <img
           src="https://images.pexels.com/photos/16350426/pexels-photo-16350426/free-photo-of-a-knife-and-a-cabbage-on-a-cutting-board.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt=""
-          className="h-[80vh] w-full object-cover "
+          className="h-[60vh] sm:h-[80vh] w-full object-cover"
         />
 
-        <div className="absolute top-[50%] left-[10%] translate-x-[50%] translate-y-[50%]">
-            <div className="flex gap-1">
-                <input type="text" placeholder="Search..." 
-                className=" border-none px-4 py-2 w-[35vw] focus:outline-none rounded-lg"
+        <div className="absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%]">
+            <div className="flex gap-1 flex-col sm:flex-row">
+                <input 
+                    type="text" 
+                    placeholder="Search..." 
+                    className="w-[70vw] sm:w-[35vw] border-none px-4 py-2 focus:outline-none rounded-lg"
                 />
-                <button className="py-2 px-4 text-white  bg-pink-600 hover:bg-pink-700 rounded-md">Search</button>
+                <button className="py-2 px-4 mt-2 sm:mt-0 text-white bg-pink-600 hover:bg-pink-700 rounded-md">Search</button>
             </div>
         </div>
       </section>
 
+      <section className="w-full flex flex-col justify-center items-center p-5">
+  <h2 className="text-2xl sm:text-3xl font-bold mb-6 sm:mb-8 text-pink-600">
+    Explore Food
+  </h2>
+
+  
+  <div className="w-full flex flex-wrap items-center justify-center gap-16">
+             {
+               foodCards.map((item) => (
+                 <FoodCards key={item.id} name={item.name} image={item.imgUrl} />
+               ))
+             }
+         </div>
+</section>
 
 
-     <section className="w-[100vw] flex flex-col justify-center items-center p-5">
-        <h2 className="text-3xl font-bold text-gray-700 mb-8 text-pink-600">
-            Explore Food
-        </h2>
-        <div className="w-full flex items-center justify-center gap-10">
-            {
-              foodCards.map((item) => (
-                <FoodCards key={item.id} name={item.name} image={item.imgUrl} />
-              ))
-            }
-        </div>
-     </section>
+      {/* MenuCards Section */}
+      <section className="mx-auto mt-10">
+        <h2 className="text-center font-bold text-2xl text-pink-600">Food Menu</h2>
 
-        <section className="mx-auto mt-10">
-          <h2 className="text-center font-bold text-2xl text-pink-600">Food Menu</h2>
-       
-       <div className="flex flex-wrap justify-around mt-10 gap-4">
+        <div className="flex flex-wrap justify-around mt-10 gap-4">
         {
           menuCards.map((item) => (
             <MenuCards key={item.id} name={item.name} price={item.price} image={item.imgURL} />
-           ) )
+          ))
         }
-          </div>
-          </section>
+        </div>
+      </section>
     </div>
   );
 }
@@ -121,33 +125,108 @@ export default Home;
 
 
 
-// import React from 'react'
 
-// function Search() {
+
+
+
+// import React from "react";
+// import FoodCards from "./FoodCards";
+// import MenuCards from "./MenuCards";
+
+// function Home() {
+//     const foodCards = [
+//         {
+//             id: 1,
+//             imgUrl: "https://plus.unsplash.com/premium_photo-1673439304183-8840bd0dc1bf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGl6emF8ZW58MHx8MHx8fDA%3D",
+//             name: "Pizza"
+            
+//         } ,
+//         {
+//             id: 2,
+//             imgUrl: "https://images.unsplash.com/photo-1485962398705-ef6a13c41e8f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZvb2QlMjBpbWFnZXMlMjBidXJndXJ8ZW58MHx8MHx8fDA%3D",
+//             name: "Pasta"
+            
+//         },
+//          {
+//             id: 3,
+//             imgUrl: "https://images.unsplash.com/photo-1552611052-d59a0d9741bc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmVzdGF1cmFudCUyMGZvb2QlMjBpbWFnZXN8ZW58MHx8MHx8fDA%3D",
+//             name: "Food"
+            
+//         }
+//     ]
+//     const menuCards = [
+//       {
+//         id: 1,
+//         name: "Pizza",
+//         price: "600",
+//         imgURL: "https://images.pexels.com/photos/19239118/pexels-photo-19239118/free-photo-of-fresh-pepperoni-sausage-pizza.jpeg?auto=compress&cs=tinysrgb&w=600"
+//       },
+//       {
+//         id: 2,
+//         name: "Pasta",
+//         price: "400",
+//         imgURL: "https://images.pexels.com/photos/24869092/pexels-photo-24869092/free-photo-of-a-plate-of-pasta-with-mushrooms-and-wine.jpeg?auto=compress&cs=tinysrgb&w=600"
+//       },
+//       {
+//         id: 3,
+//         name: "K2 Burgur",
+//         price: "580",
+//         imgURL: "https://images.pexels.com/photos/327158/pexels-photo-327158.jpeg?auto=compress&cs=tinysrgb&w=600"
+//       },
+//       {
+//         id: 4,
+//         name: "Foodies",
+//         price: "230",
+//         imgURL: "https://images.pexels.com/photos/8471703/pexels-photo-8471703.jpeg?auto=compress&cs=tinysrgb&w=600"
+//       }
+//     ]
 //   return (
-//    <>
-
-//    <div class="relative  h-screen bg-cover bg-center">
-//   <img src="/bg-img.webp" alt="here img" class="w-full h-4/5" />
-
-//   <div class="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-//     <div class="text-center">
-//       <div class="flex justify-center mb-40">
-//         <input
-//           type="text"
-//           placeholder="Search..."
-//           class="w-full   px-10  text-lg border rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+//     <div>
+//       <section className="relative">
+//         <img
+//           src="https://images.pexels.com/photos/16350426/pexels-photo-16350426/free-photo-of-a-knife-and-a-cabbage-on-a-cutting-board.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//           alt=""
+//           className="h-[80vh] w-full object-cover "
 //         />
-//         <button class="px-6 py-4 bg-blue-500 text-white text-lg rounded-r-lg hover:bg-blue-600">
-//           Search
-//         </button>
-//       </div>
-//     </div>
-//   </div>
-// </div>
 
-//    </>
-//   )
+//         <div className="absolute top-[50%] left-[10%] translate-x-[50%] translate-y-[50%]">
+//             <div className="flex gap-1">
+//                 <input type="text" placeholder="Search..." 
+//                 className=" sm:w-[35vw] w-[50vw] border-none px-4 py-2 w-[35vw] focus:outline-none rounded-lg"
+//                 />
+//                 <button className="py-2 px-4 text-white  bg-pink-600 hover:bg-pink-700 rounded-md">Search</button>
+//             </div>
+//         </div>
+//       </section>
+
+
+
+//      <section className="w-[100vw] flex flex-col justify-center items-center p-5">
+//         <h2 className="text-3xl font-bold text-gray-700 mb-8 text-pink-600">
+//             Explore Food
+//         </h2>
+//         <div className="w-full flex flex-wrap items-center justify-center gap-16">
+//             {
+//               foodCards.map((item) => (
+//                 <FoodCards key={item.id} name={item.name} image={item.imgUrl} />
+//               ))
+//             }
+//         </div>
+//      </section>
+
+//         <section className="mx-auto mt-10">
+//           <h2 className="text-center font-bold text-2xl text-pink-600">Food Menu</h2>
+       
+//        <div className="flex flex-wrap justify-around mt-10 gap-4">
+//         {
+//           menuCards.map((item) => (
+//             <MenuCards key={item.id} name={item.name} price={item.price} image={item.imgURL} />
+//            ) )
+//         }
+//           </div>
+//           </section>
+//     </div>
+//   );
 // }
 
-// export default Search
+// export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 function Navbar() {
   return (
 
-    <nav  className='flex justify-between px-5 p-2'>
+    <nav  className='flex justify-between px-5 p-2 sm:flex-row flex-col'>
       <div className='flex justify-center items-center'>
-        <img src="/public/logo.jpg" alt="img-here" className='w-[60px]' />
-      <h2 className='font-bold text-gray-600 text-lg text-pink-600'>wow food</h2>
+        <img src="/public/logo.jpg" alt="img-here" className='w-[60px]' /> 
+      <h2 className='font-bold text-lg text-pink-600'>wow food</h2>
       </div>
 
      <div className='flex gap-x-5 justify-center items-center'>
 
-      <h2 className='text-pink-600 hover:text-pink-800 hover:underline hover:cursor-pointer'>Home</h2>
-      <h2 className='text-pink-600 hover:text-pink-800 hover:underline hover:cursor-pointer'>About</h2>
-      <h2 className='text-pink-600 hover:text-pink-800 hover:underline hover:cursor-pointer'>Food</h2>
-      <h2 className='text-pink-600 hover:text-pink-800 hover:underline hover:cursor-pointer'>Contact</h2>
+      <h2 className='text-pink-600 font-bold hover:text-pink-800 hover:underline hover:cursor-pointer'>Home</h2>
+      <h2 className='text-pink-600 font-bold hover:text-pink-800 hover:underline hover:cursor-pointer'>About</h2>
+      <h2 className='text-pink-600 font-bold hover:text-pink-800 hover:underline hover:cursor-pointer'>Food</h2>
+      <h2 className='text-pink-600 font-bold hover:text-pink-800 hover:underline hover:cursor-pointer'>Contact</h2>
      </div>
 
     </nav>
@@ -22,108 +22,3 @@ function Navbar() {
 }
 
 export default Navbar
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import logo from '/public/logo.jpg'
-
-// const Navbar = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <> 
-//     <nav className="bg-gray-500 p-4">
-//       <div className="container mx-auto flex justify-between items-center ml-19">
-//         <div className="text-white text-xl font-bold flex items-center">
-//           <img src={logo} alt=""  className='w-16 h-12 flex ml-20 space-3 px-1 py-1   rounded-lg' />
-//           wowfood
-//         </div>
-//         <div className="md:hidden">
-//           <button
-//             onClick={toggleMenu}
-//             className="text-white focus:outline-none"
-//           >
-//             <svg
-//               className="w-6 h-6"
-//               fill="none"
-//               stroke="currentColor"
-//               viewBox="0 0 24 24"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path
-//                 strokeLinecap="round"
-//                 strokeLinejoin="round"
-//                 strokeWidth="2"
-//                 d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
-//               ></path>
-//             </svg>
-//           </button>
-//         </div>
-//         <ul className={`md:flex md:space-x-6 ${isOpen ? 'block' : 'hidden'} md:block`}>
-//           <li className="border-b border-gray-400 md:border-none">
-//             <a href="#" className="text-white hover:text-gray-300 block py-2 md:py-0 md:inline">
-//               Home
-//             </a>
-//           </li>
-//           <li className="border-b border-gray-400 md:border-none">
-//             <a href="#" className="text-white hover:text-gray-300 block py-2 md:py-0 md:inline">
-//               About
-//             </a>
-//           </li>
-//           <li className="border-b border-gray-400 md:border-none">
-//             <a href="#" className="text-white hover:text-gray-300 block py-2 md:py-0 md:inline">
-//               Item
-//             </a>
-//           </li>
-//           <li className="border-b border-gray-400 md:border-none">
-//             <a href="#" className="text-white hover:text-gray-300 block py-2 md:py-0 md:inline">
-//               Contact
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-
-
-
-
-
-
-
-
-// </>
-
-//   );
-// };
-
-// export default Navbar;
